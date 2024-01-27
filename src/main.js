63886081e6cca2c9008d823c6d752a2bd65b0060
Volodymyr <39493622+VolodymyrKozel@users.noreply.github.com>
2024-01-27 18:02:37 +0100

    correct

--- src/main.js
+++ src/main.js
@@ -5,42 +5,55 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form-search');
 const galleryList = document.querySelector('.gallery-list');
+const loader = document.querySelector('.loader-wrapper');
 const API_KEY = '42027651-7bedd500762feb24dffc0a2de';
-let q = '';
-const url = `https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
+const url = `https://pixabay.com/api/?`;
+const searchParams = new URLSearchParams({
+  key: API_KEY,
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+  page: 1,
+});
 
 const simplelightboxOptions = {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
 };
 const iziToastOptions = {
-        class: 'izi-toast-error-style',
-        title: 'Error',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-        backgroundColor: '#EF4040',
-        iconUrl: './img/octagon.svg',
-}
+  theme: 'dark',
+  class: 'izi-toast-error-style',
+  title: 'Error',
+  message:
+    'Sorry, there are no images matching your search query. Please try again!',
+  backgroundColor: '#EF4040',
+  iconUrl: './img/octagon.svg',
+};
+const lightbox = new SimpleLightbox('.gallery-list a', simplelightboxOptions);
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();
+  loader.style.display = 'flex';
   galleryList.innerHTML = '';
-  q = e.target.elements.search.value;
+  searchParams.set('q', e.target.elements.search.value);
   fetchImages()
-    .then(images => renderImages(images))
+    .then(images => {
+      images.totalHits > 0
+        ? renderImages(images)
+        : iziToast.error(iziToastOptions);
+      loader.style.display = 'none';
+    })
     .catch(error => console.log(error));
   form.reset();
 }
 
 function fetchImages() {
-  console.log(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  return fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
+  return fetch(url + searchParams).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -48,27 +61,35 @@ function fetchImages() {
   });
 }
 
-function renderImages(images) {
-  console.log(images);
-  if (images.hits == 0) {
-    iziToast.error(iziToastOptions);
-  } else {
-    const markup = images.hits
-      .map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => {
+const markup = images => {
+  return images
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
         return `<li class="gallery-item">
-          <a class="gallery-link" href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360" height="200"/>
-          </a>
-          <div class="item-info">
-          <p class="item-info-p">Likes <span>${likes}</span></p>
-          <p class="item-info-p">Views <span>${views}</span></p>
-          <p class="item-info-p">Comments <span>${comments}</span></p>
-          <p class="item-info-p">Downloads <span>${downloads}</span></p>
-          </div>
-          </li>`;
-      })
-      .join('');
-    galleryList.insertAdjacentHTML('beforeend', markup);
-    let lightbox = new SimpleLightbox('.gallery-list a', simplelightboxOptions);
-  }
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="360" height="200" loading="lazy"/>
+        </a>
+        <div class="item-info">
+        <p class="item-info-p">Likes <span>${likes}</span></p>
+        <p class="item-info-p">Views <span>${views}</span></p>
+        <p class="item-info-p">Comments <span>${comments}</span></p>
+        <p class="item-info-p">Downloads <span>${downloads}</span></p>
+        </div>
+        </li>`;
+      }
+    )
+    .join('');
+};
+
+function renderImages(images) {
+  galleryList.insertAdjacentHTML('beforeend', markup(images.hits));
+  lightbox.refresh();
 }
